b05bb3438e76ed7a25b3bf0dd292a54b946616df
Shantanu <shan7030666366@gmail.com>
2021-02-02 02:59:59 +0530

    :sparkles: Added the default configurations

---
+++ code_opener_cli/utils/config.py
@@ -0,0 +1,15 @@
+"""
+This file contains configuration defaults for the project 
+"""
+
+class DefaultConfiguration:
+    """
+    This class includes all the default values for the application
+    """
+
+    APP_NAME = 'code_opener_cli'
+    CONFIGURATION_DATA = {
+        'default_editor':  'code',
+        'projects': []
+    }
+    CONFIGURATION_FILE_NAME = 'settings.json'
