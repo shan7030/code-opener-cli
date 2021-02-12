c59a025ed8a8958f1b9c82946ed385667785adbf
Shantanu <shan7030666366@gmail.com>
2021-02-12 19:53:50 +0530

    :hammer: Change the config file name extention,removed configuration data

--- code_opener_cli/utils/config.py
+++ code_opener_cli/utils/config.py
@@ -1,15 +1,12 @@
 """
 This file contains configuration defaults for the project 
 """
+from enum import Enum
 
-class DefaultConfiguration:
+class DefaultConfiguration(Enum):
     """
     This class includes all the default values for the application
     """
 
     APP_NAME = 'code_opener_cli'
-    CONFIGURATION_DATA = {
-        'default_editor':  'code',
-        'projects': []
-    }
-    CONFIGURATION_FILE_NAME = 'settings.txt'
+    CONFIGURATION_FILE_NAME = 'settings.json'
